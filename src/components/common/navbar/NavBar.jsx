beda0577eb096028d4dbe6a2eb3ee2d6e01112fc
adhamh <adham.hadeed@yahoo.com>
2021-02-06 19:28:44 +0200

    add user name in navbar

--- src/components/common/navbar/NavBar.jsx
+++ src/components/common/navbar/NavBar.jsx
@@ -7,6 +7,10 @@ function NavBar(props) {
       <div className="menu-icon">
         <i className="fa fa-bars"></i>
       </div>
+      <div className="user-info">
+        <i className="fa fa-user-circle-o"></i>
+        <span>Welcome Adham Hadeed</span>
+      </div>
     </nav>
   );
 }
